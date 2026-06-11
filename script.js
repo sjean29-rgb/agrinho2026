44816e0ed955b839a9a8fc6dad40c20ab55453f2
sjean29-rgb <s.jean29@escola.pr.gov.br>
2026-06-11 12:20:15 -0300

    1

---
+++ script.js
@@ -0,0 +1,4 @@
+// Função para simular o Agrinho conversando com o produtor
+function boasVindas() {
+    alert("🌾 Agrinho diz: Olá, amigo produtor! Fique atento às nossas previsões tecnológicas e aproveite o portal para cotar seus insumos com segurança!");
+}
